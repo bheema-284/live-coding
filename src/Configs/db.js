7596f2a3c499e0ae952df4be68084127aed10ec6
bheema-284 <bheemudug@gmail.com>
2022-07-07 19:21:50 +0530

    db.js updated

--- src/Configs/db.js
+++ src/Configs/db.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb://localhost:27017/cartdata",
+    "mongodb://localhost:27017/cartdata", // we can use mongoDB atlas url as well 
   );
 };
 
